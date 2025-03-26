65a9cede8560fd7e36558b28464ba202d6e49b46
fharleston2 <168579170+fharleston2@users.noreply.github.com>
2025-03-27 04:24:47 +1000

    update for render v2

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
-<?php   
-    
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-    $method = $_SERVER['REQUEST_METHOD'];
+<?php
 
-    if ($method === 'OPTIONS') {
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-        exit();
-    }
-    
+declare(strict_types = 1);
 
+echo '<pre>';
+print_r(getenv('SITE_URL'));
+echo '<br>';
+print_r($_SERVER);
+echo '</pre>';
+
+phpinfo();
